b9bbac1850496bf94bdf5ef3f4116db8d46d01b0
Petr Sapak <petr.sapak@yahoo.com>
2024-03-13 10:38:21 +0100

    add explanatio for 1326 error code

--- src/errors.rs
+++ src/errors.rs
@@ -5,6 +5,7 @@ pub fn get_error_explanation(error_code: i32) -> String {
         67 => "Network name cannot be found".to_string(),
         86 => "Invalid password".to_string(),
         1219 => "Multiple connections to a server or shared resource by the same user, using more than one user name, are not allowed".to_string(),
+        1326 => "The username or password is incorrect".to_string(),
         _ => "Unknown error".to_string()
     }
 }
